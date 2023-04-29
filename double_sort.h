e996d232602e30d25abb537307a2ebd95a6242a2
il1egalmelon <86810482+il1egalmelon@users.noreply.github.com>
2023-04-29 19:34:18 -0400

    Update double_sort.h

--- double_sort.h
+++ double_sort.h
@@ -2,55 +2,57 @@
 #include <iostream>
 #include <vector>
 
-void merge(std::vector<double>& arr, int left, int mid, int right) {
-    int leftSize = mid - left + 1;
-    int rightSize = right - mid;
-    std::vector<double> leftArr(leftSize);
-    std::vector<double> rightArr(rightSize);
-    for (int i = 0; i < leftSize; i++) {
-        leftArr[i] = arr[left + i];
-    }
-    for (int j = 0; j < rightSize; j++) {
-        rightArr[j] = arr[mid + 1 + j];
-    }
-    int i = 0;
-    int j = 0;
-    int k = left;
-    while (i < leftSize && j < rightSize) {
-        if (leftArr[i] <= rightArr[j]) {
+namespace sort {
+    void merge(std::vector<double>& arr, int left, int mid, int right) {
+        int leftSize = mid - left + 1;
+        int rightSize = right - mid;
+        std::vector<double> leftArr(leftSize);
+        std::vector<double> rightArr(rightSize);
+        for (int i = 0; i < leftSize; i++) {
+            leftArr[i] = arr[left + i];
+        }
+        for (int j = 0; j < rightSize; j++) {
+            rightArr[j] = arr[mid + 1 + j];
+        }
+        int i = 0;
+        int j = 0;
+        int k = left;
+        while (i < leftSize && j < rightSize) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k] = leftArr[i];
+                i++;
+            }
+            else {
+                arr[k] = rightArr[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < leftSize) {
             arr[k] = leftArr[i];
             i++;
+            k++;
         }
-        else {
+        while (j < rightSize) {
             arr[k] = rightArr[j];
             j++;
+            k++;
         }
-        k++;
     }
-    while (i < leftSize) {
-        arr[k] = leftArr[i];
-        i++;
-        k++;
-    }
-    while (j < rightSize) {
-        arr[k] = rightArr[j];
-        j++;
-        k++;
-    }
-}
 
-void mergeSort(std::vector<double>& arr, int left, int right) {
-    if (left < right) {
-        int mid = (left + right) / 2;
-        mergeSort(arr, left, mid);
-        mergeSort(arr, mid + 1, right);
-        merge(arr, left, mid, right);
+    void mergeSort(std::vector<double>& arr, int left, int right) {
+        if (left < right) {
+            int mid = (left + right) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
     }
-}
 
-std::vector<double> double_sort(std::vector<double> x) {
-    std::vector<double> arr = x;
-    mergeSort(arr, 0, arr.size() - 1);
-    // for (int i = 0; i < arr.size(); i++);
-    return arr;
+    std::vector<double> dsort(std::vector<double> x) {
+        std::vector<double> arr = x;
+        mergeSort(arr, 0, arr.size() - 1);
+        // for (int i = 0; i < arr.size(); i++);
+        return arr;
+    }
 }
