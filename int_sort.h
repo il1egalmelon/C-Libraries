fbcc1a17f5ccc0fee53787754f0dafae92d4e66a
il1egalmelon <86810482+il1egalmelon@users.noreply.github.com>
2023-04-29 19:34:59 -0400

    Update int_sort.h

--- int_sort.h
+++ int_sort.h
@@ -2,32 +2,34 @@
 #include <iostream>
 #include <vector>
 
-void quicksort(std::vector<int>& arr, int low, int high) {
-    if (low < high) {
-        int pivot = arr[(low + high) / 2];
-        int left = low;
-        int right = high;
-        while (left <= right) {
-            while (arr[left] < pivot) {
-                left++;
+namespace sort {
+    void quicksort(std::vector<int>& arr, int low, int high) {
+        if (low < high) {
+            int pivot = arr[(low + high) / 2];
+            int left = low;
+            int right = high;
+            while (left <= right) {
+                while (arr[left] < pivot) {
+                    left++;
+                }
+                while (arr[right] > pivot) {
+                    right--;
+                }
+                if (left <= right) {
+                    std::swap(arr[left], arr[right]);
+                    left++;
+                    right--;
+                }
             }
-            while (arr[right] > pivot) {
-                right--;
-            }
-            if (left <= right) {
-                std::swap(arr[left], arr[right]);
-                left++;
-                right--;
-            }
+            quicksort(arr, low, right);
+            quicksort(arr, left, high);
         }
-        quicksort(arr, low, right);
-        quicksort(arr, left, high);
     }
-}
 
-std::vector<int> sort_main(std::vector<int> x) {
-    std::vector<int> arr = x;
-    quicksort(arr, 0, arr.size() - 1);
-    // for (int i = 0; i < arr.size(); i++);
-    return arr;
+    std::vector<int> isort(std::vector<int> x) {
+        std::vector<int> arr = x;
+        quicksort(arr, 0, arr.size() - 1);
+        // for (int i = 0; i < arr.size(); i++);
+        return arr;
+    }
 }
